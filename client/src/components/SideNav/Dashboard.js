85fce3224e4bd95323ba3eb51129f79f9eb041e5
Brandon Hill <bhill010@gmail.com>
2018-02-17 12:21:17 -0800

    Add styling to dashboard component

--- client/src/components/SideNav/Dashboard.js
+++ client/src/components/SideNav/Dashboard.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
 
 import "../../style/SideNav/SideNav.css";
 import "../../style/App.css";
 import "../../style/index.css";
+import "../../style/SideNav/HighScores.css";
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="index">
-        <div>
-          DASHBOARD PAGE
+        <div className="index-container">
+          <div className="index-header">
+            { this.props.auth.user.username }'s Dashboard
+          </div>
+          <div className="index-subheader">
+            Your High Score is { this.props.auth.user.highscore } points
+          </div>
+          <Link className="btn btn-primary index-button" to="/">
+            Back to Homepage
+          </Link>
         </div>
-        <Link className="btn btn-success top_margin" to="/">
-          Back to /
-        </Link>
       </div>
     )
   }
 }
 
-export default Dashboard;
+function mapStateToProps(state) {
+  return { points: state.points, auth: state.auth };
+}
+
+export default connect(mapStateToProps)(
+Dashboard
+);
