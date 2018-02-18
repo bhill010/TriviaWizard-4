c788c0986add58c0b6a82c5785e4cb6e2e31d58b
Brandon Hill <bhill010@gmail.com>
2018-02-17 21:22:26 -0800

    Add descriptions to game mode component

--- client/src/components/Trivia/GameMode.js
+++ client/src/components/Trivia/GameMode.js
@@ -57,19 +57,33 @@ class GameMode extends Component {
           <h3 className="index-header">
             Which game mode would you like to play?
           </h3>
-          <div className="gamemode-container">
-            <div>
-              <button
-                className="btn btn-info show-link index-button"
-                onClick={this.setupChallengeMode}
-              >
-                Challenge
-              </button>
+          <div className="gamemode-description-section">
+            <div className="gamemode-description-container gamemode-description-container-challenge">
+              <div className="gamemode-button-container">
+                <button
+                  className="btn btn-info show-link gamemode-button"
+                  onClick={this.setupChallengeMode}
+                >
+                  Challenge
+                </button>
+              </div>
+              <ul className="gamemode-description">
+                <li className="gamemode-description-item">60 second timer</li>
+                <li className="gamemode-description-item">Points earned for every correct answer</li>
+                <li className="gamemode-description-item">Cannot choose to have the answer revealed</li>
+              </ul>
             </div>
-            <div>
-              <Link className="btn btn-info index-button" to="/questions">
-                Casual
-              </Link>
+            <div className="gamemode-description-container">
+              <div className="gamemode-button-container">
+                <Link className="btn btn-info index-button" to="/questions">
+                  Casual
+                </Link>
+              </div>
+              <ul className="gamemode-description">
+                <li className="gamemode-description-item">Unlimited time</li>
+                <li className="gamemode-description-item">No points, just brushing up your trivia knowledge</li>
+                <li className="gamemode-description-item">Answel reveal option made available</li>
+              </ul>
             </div>
           </div>
         </div>
